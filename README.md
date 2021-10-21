0c2b49fddee50485dc5e52642b86fc846f381b9d
1747836307@qq.com <chris.561@sjtu.edu.cn>
2021-10-21 22:00:20 +0800

    创建初始化云端项目

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # backend-cloud
+
 项目云端的后端代码
---
+++ src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/EcssBackendCloudApplication.java
@@ -0,0 +1,13 @@
+package ist.sjtu.edu.cn.ecssbackendcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcssBackendCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcssBackendCloudApplication.class, args);
+    }
+
+}
