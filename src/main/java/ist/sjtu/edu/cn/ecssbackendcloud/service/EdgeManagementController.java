200012631e388229db89444cea334853d0184846
Fengming-Zhang <zfmfengming@mail.ustc.edu.cn>
2021-11-03 23:47:52 +0800

    update: add API for edge-register.

--- src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/service/EdgeManagementController.java
+++ src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/service/EdgeManagementController.java
@@ -1,7 +1,25 @@
 package ist.sjtu.edu.cn.ecssbackendcloud.service;
 
 
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@CrossOrigin
 public class EdgeManagementController {
+
+    @GetMapping("/edge/list")
+    public void getEdgeList() {
+
+    }
+
+    @GetMapping("/edge")
+    public void getEdgeInfo(@RequestParam("edgeId") Long edgeId) {
+
+    }
+
+    @PostMapping("/edge/register")
+    public void registerEdge() {
+        
+    }
 
 }
