a1ebf91d5978443c85d8f9a0a496e76f991ca896
Fengming-Zhang <zfmfengming@mail.ustc.edu.cn>
2021-11-08 22:41:58 +0800

    update: add dao for edge info.

---
+++ src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/dao/EdgeInfoDao.java
@@ -0,0 +1,8 @@
+package ist.sjtu.edu.cn.ecssbackendcloud.dao;
+
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EdgeInfoDao implements JpaRepositoriesAutoConfiguration {
+}
