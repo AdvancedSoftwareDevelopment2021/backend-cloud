4ad572f6e98ebd2d59d2b4b330f1312bd28350c7
Fengming-Zhang <zfmfengming@mail.ustc.edu.cn>
2021-11-09 22:47:07 +0800

    update: config for mongoDB.

--- src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/dao/EdgeInfoDao.java
+++ src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/dao/EdgeInfoDao.java
@@ -4,5 +4,5 @@ import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfig
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EdgeInfoDao implements JpaRepositoriesAutoConfiguration {
+public interface EdgeInfoDao implements MongoRepository<> {
 }
