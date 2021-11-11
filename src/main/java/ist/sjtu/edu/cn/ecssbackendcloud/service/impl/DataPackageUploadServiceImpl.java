73c700cfe0aa96f23b15281cd054d1b08cb89a83
Fengming-Zhang <zfmfengming@mail.ustc.edu.cn>
2021-11-11 23:34:32 +0800

    update: add service implement for data package management.

---
+++ src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/service/impl/DataPackageUploadServiceImpl.java
@@ -0,0 +1,16 @@
+package ist.sjtu.edu.cn.ecssbackendcloud.service.impl;
+
+import ist.sjtu.edu.cn.ecssbackendcloud.service.DataPackageUploadService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DataPackageUploadServiceImpl implements DataPackageUploadService {
+
+    @Override
+    public String uploadDataPackage() {
+        return "";
+    }
+
+}
