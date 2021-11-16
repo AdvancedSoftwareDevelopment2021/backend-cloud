00f6c4c1b5f4384a4b00fb41bc86bda9cb1c67e1
Fengming-Zhang <zfmfengming@mail.ustc.edu.cn>
2021-11-16 23:19:00 +0800

    update: add dao for data package.

---
+++ src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/dao/DataPackageInfoDao.java
@@ -0,0 +1,8 @@
+package ist.sjtu.edu.cn.ecssbackendcloud.dao;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DataPackageInfoDao implements MongoRepository<> {
+}
