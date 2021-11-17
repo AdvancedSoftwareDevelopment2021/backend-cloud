5f8402c1981f266472cb36e2ad43fb51697046fb
Fengming-Zhang <zfmfengming@mail.ustc.edu.cn>
2021-11-17 23:41:17 +0800

    update: config for dao.

--- src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/dao/DataPackageInfoDao.java
+++ src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/dao/DataPackageInfoDao.java
@@ -4,5 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DataPackageInfoDao implements MongoRepository<> {
+public interface DataPackageInfoDao extends MongoRepository<DataPackageInfoDao, Long> {
 }
--- src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/dao/EdgeInfoDao.java
+++ src/main/java/ist/sjtu/edu/cn/ecssbackendcloud/dao/EdgeInfoDao.java
@@ -1,8 +1,11 @@
 package ist.sjtu.edu.cn.ecssbackendcloud.dao;
 
+import com.sun.javafx.geom.Edge;
+import ist.sjtu.edu.cn.ecssbackendcloud.dto.EdgeInfoDto;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EdgeInfoDao implements MongoRepository<> {
+public interface EdgeInfoDao extends MongoRepository<EdgeInfoDto, Long> {
 }
