483b4880bb1036cdf41bc42519d49bf1c663b83b
Fengming-Zhang <zfmfengming@mail.ustc.edu.cn>
2021-11-28 20:25:23 +0800

    update

--- src/main/java/cn/edu/sjtu/ist/ecssbackendcloud/utils/EdgeInfoUtil.java
+++ src/main/java/cn/edu/sjtu/ist/ecssbackendcloud/utils/EdgeInfoUtil.java
@@ -25,7 +25,7 @@ public class EdgeInfoUtil {
     public PingInfoRequest createPingBody(EdgeInfoPO edgeInfoPO) {
         PingInfoRequest pingInfoRequest = new PingInfoRequest();
         pingInfoRequest.setId(edgeInfoPO.getId());
-        pingInfoRequest.setUrl("http://10.0.0.133:8080/");
+        pingInfoRequest.setUrl("http://localhost:8080/datapackage");
         pingInfoRequest.setTimeUnit(edgeInfoPO.getTimeUnit());
         pingInfoRequest.setInterval(edgeInfoPO.getInterval());
         return pingInfoRequest;
